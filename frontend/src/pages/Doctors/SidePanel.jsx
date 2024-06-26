157a8f24c4743598abf37fca7e6e92989ed507ec
Aditya Gupta <adityagupta11032003@gmail.com>
2024-06-26 11:59:04 +0530

    second

--- frontend/src/pages/Doctors/SidePanel.jsx
+++ frontend/src/pages/Doctors/SidePanel.jsx
@@ -33,7 +33,7 @@ const SidePanel = ({doctorId, ticketPrice, timeSlots}) => {
       <div className="flex items-center justify-between">
         <p className="text_para mt-0 font-semibold">Ticket Price</p>
         <span className="text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold">
-          {ticketPrice} BDT
+          {ticketPrice} INR
         </span>
       </div>
       <div className="mt-[30px]">
